1888d04adcc847296aeb56765afcd7d839b78ae9
christian69 <christianisapro28@gmail.com>
2025-07-18 10:25:44 +0000

    Jul 18, 2025, 10:25 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -23,11 +23,6 @@ function draw() {
     // ellipse(400,300,200,200);
     // ellipse(400,300,50,100);
   // Challenge 2: A face using circle() function
-        ellipse(400,300,600,500);
-        ellipse(220,50,150,150);
-        ellipse(610,50,150,150);
-
-
         //     ellipse(400,300,500,400);
         // ellipse(220,75,150,150);
         // ellipse(610,90,150,150);
