4621f7dad3d2663dd7ec7ff3f2aa9a037bef73f2
christian69 <christianisapro28@gmail.com>
2025-07-18 10:39:43 +0000

    Jul 18, 2025, 10:39 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -28,7 +28,7 @@ function draw() {
          ellipse(610,90,150,150);
     
   // Challenge 3: A house using rect() function
-         rect(150,300,300,500)
+         rect(150,300,500,0)
   // Challenge 4: A rocketship using triangle() function
 
   // Challenge 5: A square face using quad() function
