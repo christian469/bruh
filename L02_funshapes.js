2b7d93e03e450dbb9cd449608f2ceb671dffc3e2
christian69 <christianisapro28@gmail.com>
2025-07-18 10:09:37 +0000

    Jul 18, 2025, 10:09 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -20,7 +20,7 @@ Task 1: Add the setup() function
 function draw() {
   // Challenge 1: An eye using ellipse() function
     ellipse(400,300,300,200);
-    ellipse(400,200,90,200);
+    ellipse(400,300,90,200);
     ellipse(400,300,100,200);
      
 
