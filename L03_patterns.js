15ff9f128e9bc152e1138a3b282a72090f46987d
christian69 <christianisapro28@gmail.com>
2025-07-25 10:11:35 +0000

    Jul 25, 2025, 10:11 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -24,7 +24,7 @@ console.log(c);
 function draw() {
   // Recap 1: Repeating Circles
     // circle(100,100,50);
-    // circle
+    // circle(100,100,50)
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
