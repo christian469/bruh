6f51d5561897e1493d41b3fb2a6653fe0692660f
christian69 <christianisapro28@gmail.com>
2025-07-25 10:28:54 +0000

    Jul 25, 2025, 10:28 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -31,16 +31,7 @@ function draw() {
     for(let i = 1; i <= 20; i++){
         circle(100 * i, 100, 50);
     }
-
-        if (i % 2 === 0){
-         fill(0);
-        
-        }
-        else{
-          fill(225); //gray
-        }
-
-        circle(50 * i, 25, 50);
+      
 
 
 
