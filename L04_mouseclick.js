1e501f0b79061752a34c190e866d4254daef266f
christian69 <christianisapro28@gmail.com>
2025-08-01 09:46:09 +0000

    Aug 1, 2025, 9:46 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,7 +1,11 @@
-// write your codes here
+
 function setup() {
      createCanvas(400, 400);
      background(220);
 }
 
-function draw()  {}
+function draw() {
+    for (let i = 0; 5; i++){
+         circle( 50, 400, 100)
+    }
+}
