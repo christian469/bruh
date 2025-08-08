94935adbffec6d3c4344e0e13c7ac33a3f8eef6e
christian69 <christianisapro28@gmail.com>
2025-08-08 09:48:50 +0000

    Aug 8, 2025, 9:48 AM

--- L04_paintbrush.js
+++ L04_paintbrush.js
@@ -2,13 +2,15 @@ function setup() {
      createCanvas(1000, 1000);
      // background('gray');
 }
-// function mousePressed(){
-//     // evertime mouse pressed, this code runs
-// //     shapeColor = 'red';
-//     shapeColor = color(random(255), random(225), 255);
-//     random(255)
+function mousePressed(){
 
-function mousePressed()
+
+function mousePressed(){
+    // evertime mouse pressed, this code runs
+//     shapeColor = 'red';
+    shapeColor = color(random(255), random(225), 255);
+    random(255)
+}
 
 function mouseDragged(){
     shapeColor = color(random(255), random(225), 255);
