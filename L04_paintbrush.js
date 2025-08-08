a5ea70d57430b4b89811dae83f3c1b7f89447eaf
christian69 <christianisapro28@gmail.com>
2025-08-08 09:53:39 +0000

    Aug 8, 2025, 9:53 AM

--- L04_paintbrush.js
+++ L04_paintbrush.js
@@ -18,5 +18,5 @@ function mouseDragged(){
     shapeColor = color(random(255), random(225), 255);
     fill(shapeColor);
     noStroke();
-    circle(5, width / 2, height/ 2, diameter);
+    circle(mouse, width / 2, height/ 2, diameter);
 }
