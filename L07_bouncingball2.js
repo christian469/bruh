476b96619aa624b9400bc667c511c0602e9907ec
christian69 <christianisapro28@gmail.com>
2025-08-29 09:44:54 +0000

    Aug 29, 2025, 9:44 AM

---
+++ L07_bouncingball2.js
@@ -0,0 +1,36 @@
+let ballX = 300;
+let ballY = 200;
+let ballXspeed = 9;
+let ballYspeed = 9;
+let ballWidth = 30;
+
+
+
+function setup(){
+    createCanvas(600, 400);
+}
+
+
+function draw(){
+    // background('black');
+    noStroke();
+  
+
+ballX = ballX + ballXspeed;
+
+
+if (ballX + ballWidth/ 2>= width || ballX - ballWidth/ 2 <= 0){
+    fill(random(255), random(255), random(255));
+    ballXspeed = ballXspeed * -1;
+}
+
+
+ballY = ballY + ballYspeed;
+if (ballY + ballWidth/ 2 >= height || ballY - ballWidth/ 2 <= 0){
+    fill(random(255), random(255), random(255));
+    ballYspeed = ballYspeed * -1;
+}
+
+
+    circle(ballX, ballY, ballWidth);
+}
