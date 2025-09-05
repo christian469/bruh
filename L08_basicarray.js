44282d13eb71c40ab1fdabb0a66168e93fd7d242
christian69 <christianisapro28@gmail.com>
2025-09-05 10:51:35 +0000

    Sep 5, 2025, 10:51 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -32,8 +32,7 @@ function  setup(){
         console.log(favFood[i]);
     }
     text(favFood[i], width/ 2, 100 + i * 50);
-    textSize(30)
-    textAlign(centre, centre)
+    
 
 function draw(){
 }
