6be1d046573eaefa3d185b64728cc9699835625d
christian69 <christianisapro28@gmail.com>
2025-09-05 10:22:39 +0000

    Sep 5, 2025, 10:22 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,7 +1,7 @@
 // write your codes here
 let starwars;
 let boolFlag;
-let fav
+let favFood;
 // preload function
 function preload(){
     starwars = loadSound("assets/star_wars_theme_8_bit.mp3");
