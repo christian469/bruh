59ec706acb1f6b3c52153a280949c35650fe58aa
christian69 <christianisapro28@gmail.com>
2025-09-05 10:13:30 +0000

    Sep 5, 2025, 10:13 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,42 +1,45 @@
 // write your codes here
-let starwars;
-let boolFlag;
+// let starwars;
+// let boolFlag;
 
-// preload function
-function preload(){
-    starwars = loadSound("assets/star_wars_theme_8_bit.mp3");
-}
+// // preload function
+// function preload(){
+//     starwars = loadSound("assets/star_wars_theme_8_bit.mp3");
+// }
 
-// setup()
-function  setup(){
-    createCanvas(600, 400);
-    background("brown");
+// // setup()
+// function  setup(){
+//     createCanvas(600, 400);
+//     background("brown");
 
-    boolFlag = false;
+//     boolFlag = false;
 
-    fill('white');
-    textSize(32);
-    text("GOODBYE WORLD", width/2, height/ 2);
-}
+//     fill('white');
+//     textSize(32);
+//     text("GOODBYE WORLD", width/2, height/ 2);
+// }
 
-function draw(){
+// function draw(){
 
-}
+// }
 
 
-function mousePressed(){
-    if (boolFlag){
-        starwars.play();
-    }
+// function mousePressed(){
+//     if (boolFlag){
+//         starwars.play();
+//     }
         
-    else{
-        starwars.stop();
-    }
+//     else{
+//         starwars.stop();
+//     }
 
-    boolFlag = !boolFlag
-}
+//     boolFlag = !boolFlag
+// }
 
 // function mouseReleased(){
 //     starwars.stop();
 // }
+
+
+
 
