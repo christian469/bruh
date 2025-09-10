2013d9f22d60b9017ee7bdeeaade022dd29cc11c
christian69 <christianisapro28@gmail.com>
2025-09-10 08:15:48 +0000

    Sep 10, 2025, 8:15 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -17,9 +17,9 @@ function  setup(){
     fill('black');
     textSize(30);
     textAlign(CENTER, CENTER);
-    text("Uncle Roger",50, 50);
-    text("18",50, 100);
-    text("killing black people",50, 150);
+    // text("Uncle Roger",50, 50);
+    // text("18",50, 100);
+    // text("killing black people",50, 150);
 
   
     favFood = ["human blood", "pasta", "dogs", "human meat", "cork"];
