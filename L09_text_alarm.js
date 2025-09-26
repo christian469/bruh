f5225a7abb929376ccb269e32cfce22897b32255
christian69 <christianisapro28@gmail.com>
2025-09-26 10:00:28 +0000

    Sep 26, 2025, 10:00 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -10,6 +10,6 @@ function draw(){
     csecond = second();
     cminute = minute();
     chour = hour();
-    ctime = cminute + " : " + csecond
+    ctime = chour + " : " cminute + " : " + csecond
     text(ctime, width/ 2, height/ 2);
 }
