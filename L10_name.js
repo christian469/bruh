972f6a567b73d1d71f38ffafd8d1d5eb7154b8e9
christian69 <christianisapro28@gmail.com>
2025-10-03 10:19:51 +0000

    Oct 3, 2025, 10:19 AM

--- L10_name.js
+++ L10_name.js
@@ -11,6 +11,6 @@ function draw(){
     background('darkred')
     fill("white");
     textSize(24);
-    textAlign(CustomElementRegistry, CENT)
+    textAlign(CENTER)
     text(displayText, width/ 2, height/ 2);
 }
