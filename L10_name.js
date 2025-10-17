a2974281a85241c3a08427e2c05409f5b3835b0a
christian69 <christianisapro28@gmail.com>
2025-10-17 09:24:01 +0000

    Oct 17, 2025, 9:24 AM

--- L10_name.js
+++ L10_name.js
@@ -1,37 +1,56 @@
 let displayText;
-let inputText;
-let inputName;
+let displayName;
 let displayAge;
+
+let inputName;
+let inputAge;
+
+let colorPicker;
+
 
 function setup(){
     createCanvas(600,400)
-    displayText = "Let Diddy sharpen ur pencil XD"
+    // displayText = "Let Diddy sharpen ur pencil XD"
 
-    inputText = createInput();
-    inputText.position(100, height - 100);
-    inputText.input(updateText);
+    inputName = createInput();
+    inputName.position(300, height - 150);
+    inputName.input(updateName);
 
-     inputName = createInput();
-    inputName.position(105, height - 150);
-    inputName.input(updateText);
+     inputAge = createInput();
+    inputAge.position(300, height - 100);
+    inputAge.input(updateAge);
+
+    colorPicker = createColorPicker("yellow");
+   colorPicker.position(300, height - 50) 
 
 }
 
 function draw(){
-    background('darkred')
-    fill("white");
+    background(colorPicker.value())
+    rect(150,50,300,150,10);
+    
+    push();
     textSize(19);
-    textAlign(CENTER,CENTER)
-    text(displayText, width/2, height/2);
-    text(displayAge, width/2, height/2)
+    textAlign(LEFT,TOP);
+    text("Enter your name:", 50, height- 150); 
+    text("Enter your age", 50, height - 100);
+    text("Choose a color:", 50, height - 50);
 
-    text("Name", 80, 259);
-    text("Age", 80, 310)
+    pop();
+    
+    push()
+    textSize(32);
+    textAlign(CENTER,CENTER);
+    text(displayName, width/2, height-300);
+    text(displayAge, width/2, height-250);
+    pop();
 }
 
 
+function updateName(){
+    displayName = this.value();
+}
 
-
-function updateText(){
-    displayText = this.value();
+function updateAge(){
+    displayAge = this.value();
 }
--- L11_madlib.js
+++ L11_madlib.js
@@ -1,1 +1,46 @@
-// write your codes here
+let inputNoun, inputVerb, inputAdjective, inputAdverb, inputPlace;
+let btnGenStory
+
+
+function setup(){
+    createCanvas(600,600);
+    inputNoun = createInput("e.g. Tytus");
+    inputNoun.position(200, 100);
+
+    inputVerb = createInput("e.g. swim");
+    inputVerb.position(200,150);
+
+    inputAdjective = createInput("e.g. happy");
+    inputAdjective.position(200, 200);
+
+    inputAdverb = createInput("e.g. happily");
+    inputAdverb.position(200, 250);
+
+    inputPlace = createInput("e.g. Ang Mo Kio");
+    inputPlace.position (200, 300);
+
+    btnGenStory = createButton("Generate Story");
+    btnGenStory.position(200, 350);
+    btnGenStory.mousePressed(buttonStoryClicked)
+}
+
+function draw(){
+    background("maroon");
+
+    push();
+    fill("orange");
+    textSize(18);
+    textAlign(LEFT, TOP);
+    textFont("cosmic san")
+    text("Noun:", 50, 100);
+    text("Verb:", 50, 150);
+    text("Adjective:", 50, 200);
+    text("Adverb:", 50, 250);
+    text("Place:", 50, 300);
+    pop();
+}
+
+function buttonStoryClicked(){
+    console.log("this button will kill anyone christian choose")
+    console.log(inputNoun.value());
+}
