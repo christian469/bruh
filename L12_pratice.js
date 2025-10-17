993cf82c2539d03ea807d742222297c80233370d
christian69 <christianisapro28@gmail.com>
2025-10-17 09:54:30 +0000

    Oct 17, 2025, 9:54 AM

--- L12_pratice.js
+++ L12_pratice.js
@@ -3,7 +3,7 @@ console.log(heroes[0]);
 console.log(heroes[1]);
 console.log(heroes[2]);
 console.log(heroes[3]);
-console.log
+console.log(heroes[4])
 
 for (let i = 34; i < 57; i++){
     console.log("number: " + i);
