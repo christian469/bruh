57fbc88f319f96c723553bb148524a17e90ecab4
christian69 <christianisapro28@gmail.com>
2025-10-17 09:54:51 +0000

    Oct 17, 2025, 9:54 AM

--- L12_pratice.js
+++ L12_pratice.js
@@ -1,9 +1,10 @@
-heroes = ["bonnie", "chica", "freddy", "ballon boy", "foxy", "diddy", "sex worker"]
+heroes = ["bonnie", "chica", "freddy", "ballon boy", "foxy", "diddy", "lll worker"]
 console.log(heroes[0]);
 console.log(heroes[1]);
 console.log(heroes[2]);
 console.log(heroes[3]);
-console.log(heroes[4])
+console.log(heroes[4]);
+console.log(heroes[5])
 
 for (let i = 34; i < 57; i++){
     console.log("number: " + i);
