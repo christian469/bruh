bd584c963612b9f8574da9d4eaabe1d6ad480269
christian69 <christianisapro28@gmail.com>
2025-10-17 09:59:51 +0000

    Oct 17, 2025, 9:59 AM

--- L12_pratice.js
+++ L12_pratice.js
@@ -1,7 +1,5 @@
 heroes = ["bonnie", "chica", "freddy", "ballon boy", "foxy", "diddy", "smexyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy worker"]
 
-
-
 function setup(){
     createCanvas(400, 600);
     background('maroon')
