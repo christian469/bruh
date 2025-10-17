72708d7a8f2b60e24a769b644e6c301b30d027e4
christian69 <christianisapro28@gmail.com>
2025-10-17 10:08:12 +0000

    Oct 17, 2025, 10:08 AM

--- L12_pratice.js
+++ L12_pratice.js
@@ -5,12 +5,12 @@ function setup(){
     createCanvas(400, 600);
     background('maroon')
 
-    text(heroes[0], 100, 100);
+    
 }
 function draw(){
     for (let i = 0; i < heroes.length; i++){
     // console.log("Heroes: " +heroes[i]);
-
+    text(heroes[0], 100, 100);
 
 }
 
