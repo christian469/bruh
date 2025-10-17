7c947aef5e610b3752e22feb886974967b0a4023
christian69 <christianisapro28@gmail.com>
2025-10-17 09:51:48 +0000

    Oct 17, 2025, 9:51 AM

--- L12_pratice.js
+++ L12_pratice.js
@@ -1,4 +1,4 @@
-heroes = ["bonnie", "chica", "freddy", "ballon boy", "foxy"]
+heroes = ["bonnie", "chica", "freddy", "ballon boy", "foxy", "diddy"]
 console.log(heroes[2]);
 
 for (let i = 34; i < 57; i++){
