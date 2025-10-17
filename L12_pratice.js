f1a23cd95893aa2c975fa0b1e8a00deb18c13c36
christian69 <christianisapro28@gmail.com>
2025-10-17 10:01:48 +0000

    Oct 17, 2025, 10:01 AM

--- L12_pratice.js
+++ L12_pratice.js
@@ -1,11 +1,4 @@
 heroes = ["bonnie", "chica", "freddy", "ballon boy", "foxy", "diddy", "smexyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy worker"]
-console.log(heroes[0]);
-console.log(heroes[1]);
-console.log(heroes[2]);
-console.log(heroes[3]);
-console.log(heroes[4]);
-console.log(heroes[5]);
-console.log(heroes[6]);
 
 for (let i = 0; i < heroes.length; i++){
     console.log("Heroes: " + heroes[i]);
