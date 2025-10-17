9849b996885fb8de46e7e4cc2e5b60b3a204d505
christian69 <christianisapro28@gmail.com>
2025-10-17 09:59:30 +0000

    Oct 17, 2025, 9:59 AM

--- L12_pratice.js
+++ L12_pratice.js
@@ -1,15 +1,6 @@
 heroes = ["bonnie", "chica", "freddy", "ballon boy", "foxy", "diddy", "smexyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy worker"]
-console.log(heroes[0]);
-console.log(heroes[1]);
-console.log(heroes[2]);
-console.log(heroes[3]);
-console.log(heroes[4]);
-console.log(heroes[5]);
-console.log(heroes[6]);
 
-for (let i = 0; i < heros.length; i++){
-    console.log("Heroes: " + heroes[i]);
-}
+
 
 function setup(){
     createCanvas(400, 600);
