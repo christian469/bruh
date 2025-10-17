5da3d32ab26aa8ad11af6c53f8bfd97b4b711bf4
christian69 <christianisapro28@gmail.com>
2025-10-17 10:04:11 +0000

    Oct 17, 2025, 10:04 AM

--- L12_pratice.js
+++ L12_pratice.js
@@ -1,5 +1,5 @@
 heroes = ["bonnie", "chica", "freddy", "ballon boy", "foxy", "diddy", "smexyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy worker"]
-
+for (let i = 0; i < heroes.length; i++)
 
 function setup(){
     createCanvas(400, 600);
