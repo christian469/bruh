6902ec6927c60fb2694fbfd0316ea7887509cd37
christian69 <christianisapro28@gmail.com>
2025-10-31 10:20:25 +0000

    Oct 31, 2025, 10:20 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -60,7 +60,7 @@ function buttonStoryClicked(){
     storyText = ranstory.replace("{noun}", inputNoun.value());
     storyText = ranstory.replace("{noun}", inputVerb.value());
     storyText = ranstory.replace("{noun}", inputAdjective.value());
-    storyText = ranstory.replace("{noun}", inputNoun.value());
+    storyText = ranstory.replace("{noun}", inputAdverb.value());
     storyText = ranstory.replace("{noun}", inputNoun.value());
     storyText = ranstory.replace("{noun}", inputNoun.value());
     console.log(storyText)
