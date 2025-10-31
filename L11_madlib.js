2dc5b1e585ebbbda6796c7ec5e90b180315484d3
christian69 <christianisapro28@gmail.com>
2025-10-31 10:02:59 +0000

    Oct 31, 2025, 10:02 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -23,7 +23,7 @@ function setup(){
     btnGenStory.position(200, 350);
     btnGenStory.mousePressed(buttonStoryClicked)
 
-    storyTemplate = ["The "]
+    storyTemplate = ["The {adjec}"]
 }
 
 function draw(){
